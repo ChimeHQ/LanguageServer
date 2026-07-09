3f30c6900fbf300e74e592a436e3d53122d489f7
isla w <info@mrsdizzie.com>
2026-07-09 12:56:01 -0400

    Add typeHierarchySubtypes and typeHierarchySupertypes to default implementations (#8)
    * Add typeHierarchySubtypes and typeHierarchySupertypes to default implementations
    
    * Add a test to catch missing implementations

--- Tests/LanguageServerTests/LanguageServerTests.swift
+++ Tests/LanguageServerTests/LanguageServerTests.swift
@@ -9,4 +9,16 @@ final class LanguageServerTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+	
+	/// A compile-time check to ensure `RequestHandler` and `NotificationHandler`
+	/// continue to provide default implementations for every protocol requirement.
+	func testRequestHandlerAndNotificationHandlerHaveCompleteDefaults() throws {
+		struct MinimalRequestHandler: RequestHandler {
+			func internalError(_ error: Error) async {}
+		}
+	
+		struct MinimalNotificationHandler: NotificationHandler {
+			func internalError(_ error: Error) async {}
+		}
+	}
 }
